d7d996f3d7f4907d9d55b908ef22ef1b92cfa048
Pawel-Kosinski <p4awel123@gmail.com>
2024-11-29 16:34:37 +0100

    delete, passenger update

--- passenger.c
+++ passenger.c
@@ -24,7 +24,7 @@ void *Passenger() {
             pthread_mutex_unlock(&mutex);
             shared_data->passengers_on_board++;
             shared_data->passengers_on_bridge--;
-            printf("Pasazer %ld wchodzi na stated. Liczba osob na statku: %d\n", pthread_self(), shared_data->passengers_on_board);
+            printf("Pasazer %ld wchodzi na statek. Liczba osob na statku: %d\n", pthread_self(), shared_data->passengers_on_board);
             pthread_mutex_unlock(&mutex);
 
             if (sem_post(&bridge_sem) == -1) {
@@ -42,9 +42,32 @@ void *Passenger() {
                 sleep(1);
             }
 
+            if (sem_post(&ship_sem) == -1) {
+                perror("sem_post ship sem");
+                pthread_exit(NULL);
+            }
             pthread_mutex_lock(&mutex);
             shared_data->passengers_on_board--;
             printf("Pasazer %ld opuszcza statek. Liczba osob na statku: %d\n", pthread_self(), shared_data->passengers_on_board);
+
+            if (sem_wait(&bridge_sem) == -1) {
+                perror("sem_wait bridge sem");
+                pthread_exit(NULL);
+            }
+            pthread_mutex_lock(&mutex);
+            shared_data->passengers_on_bridge++;
+            printf("Pasazer %ld schodzi na mostek. Liczba osob na mostku: %d\n", pthread_self(), shared_data->passengers_on_bridge);
+            pthread_mutex_unlock(&mutex);
+
+            if (sem_post(&bridge_sem) == -1) {
+                perror("sem_post bridge sem");
+                pthread_exit(NULL);
+            }
+            pthread_mutex_lock(&mutex);
+            shared_data->passengers_on_bridge--;
+            printf("Pasazer %ld schodzi z mostku. Liczba osob na mostku: %d\n", pthread_self(), shared_data->passengers_on_bridge);
+            pthread_mutex_unlock(&mutex);
+
             pthread_exit(NULL);
         }
         else {
